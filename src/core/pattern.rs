269517abb0871f992319fd20968e954a01e5d48f
hyst329 <trolleybus.1329@gmail.com>
2019-03-11 11:28:34 +0300

    couple pattern stub

--- src/core/pattern.rs
+++ src/core/pattern.rs
@@ -1,8 +1,31 @@
+use std::cmp::Ordering;
 use std::collections::VecDeque;
+use std::marker::PhantomData;
 
-type Idx = u64;
+#[derive(Clone, Copy, Eq)]
+struct Idx(u64);
 
-#[derive(Clone)]
+const IDX_MODULUS: u64 = 100_000;
+
+impl Ord for Idx {
+    fn cmp(&self, other: &Idx) -> Ordering {
+        (self.0 / IDX_MODULUS).cmp(&(other.0 / IDX_MODULUS))
+    }
+}
+
+impl PartialOrd for Idx {
+    fn partial_cmp(&self, other: &Idx) -> Option<Ordering> {
+        Some((self.0 / IDX_MODULUS).cmp(&(other.0 / IDX_MODULUS)))
+    }
+}
+
+impl PartialEq for Idx {
+    fn eq(&self, other: &Idx) -> bool {
+        (self.0 / IDX_MODULUS) == (other.0 / IDX_MODULUS)
+    }
+}
+
+#[derive(Clone, Copy)]
 struct IdxValue<T>
 where
     T: Clone,
@@ -42,7 +65,7 @@ trait FieldExtractor<Event, T> {
     fn extract(&self, event: &Event, key: &str) -> T;
 }
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 enum Result<T> {
     Success(T),
     Failure,
@@ -53,11 +76,11 @@ where
     State: PatternState<T>,
     T: Clone,
 {
-    fn initial_state() -> State;
+    fn initial_state(&self) -> State;
     fn apply(&self, old_state: State, events: Vec<Event>) -> State;
 }
 
-trait PatternState<T>
+trait PatternState<T>: Clone
 where
     T: Clone,
 {
@@ -70,6 +93,7 @@ struct SimplePattern<Event, T> {
     extractor: Box<IdxExtractor<Event>>,
 }
 
+#[derive(Clone)]
 struct SimplePatternState<T>
 where
     T: Clone,
@@ -87,7 +111,7 @@ impl<T: Clone> PatternState<T> for SimplePatternState<T> {
 }
 
 impl<Event, T: Clone> Pattern<Event, SimplePatternState<T>, T> for SimplePattern<Event, T> {
-    fn initial_state() -> SimplePatternState<T> {
+    fn initial_state(&self) -> SimplePatternState<T> {
         SimplePatternState {
             queue: VecDeque::new(),
         }
@@ -123,5 +147,108 @@ fn field<Event: 'static, T: 'static + Clone>(
     SimplePattern {
         f: Box::new(move |event| Result::Success(field_extractor.extract(event, &key))),
         extractor,
+    }
+}
+
+struct CouplePattern<Event, State1, State2, T1, T2, T3>
+where
+    State1: PatternState<T1>,
+    State2: PatternState<T2>,
+    T1: Clone,
+    T2: Clone,
+    T3: Clone,
+{
+    left: Box<Pattern<Event, State1, T1>>,
+    right: Box<Pattern<Event, State2, T2>>,
+    func: Box<Fn(&Result<T1>, &Result<T2>) -> Result<T3>>,
+}
+
+#[derive(Clone)]
+struct CouplePatternState<State1, State2, T1, T2, T3>
+where
+    State1: PatternState<T1>,
+    State2: PatternState<T2>,
+    T1: Clone,
+    T2: Clone,
+    T3: Clone,
+{
+    left: Box<State1>,
+    right: Box<State2>,
+    queue: QI<T3>,
+    phantom1: PhantomData<T1>,
+    phantom2: PhantomData<T2>,
+}
+
+fn couple_inner<T1: Clone, T2: Clone, T3: Clone>(
+    mut first: QI<T1>,
+    mut second: QI<T2>,
+    mut total: QI<T3>,
+    func: Box<Fn(&Result<T1>, &Result<T2>) -> Result<T3>>,
+) -> (QI<T1>, QI<T2>, QI<T3>) {
+    let default = (first.clone(), second.clone(), total.clone());
+    match (first.get(0), second.get(0)) {
+        (Some(iv1), Some(iv2)) => {
+            let idx1 = iv1.index;
+            let val1 = &iv1.value;
+            let idx2 = iv2.index;
+            let val2 = &iv2.value;
+            if idx1 == idx2 {
+                let result = func(val1, val2);
+                first.pop_front();
+                second.pop_front();
+                total.push_back(IdxValue::new_simple(idx1, result));
+                couple_inner(first, second, total, func)
+            } else if idx1 < idx2 {
+                first.pop_front();
+                couple_inner(first, second, total, func)
+            } else {
+                second.pop_front();
+                couple_inner(first, second, total, func)
+            }
+        }
+        _ => default,
+    }
+}
+
+fn couple_process_queues<T1: Clone, T2: Clone, T3: Clone>(
+    first: QI<T1>,
+    second: QI<T2>,
+    total: QI<T3>,
+    func: Box<Fn(&Result<T1>, &Result<T2>) -> Result<T3>>,
+) -> (QI<T1>, QI<T2>, QI<T3>) {
+    couple_inner::<T1, T2, T3>(first, second, total, func)
+}
+
+impl<Event, State1, State2, T1: Clone, T2: Clone, T3: Clone>
+    Pattern<Event, CouplePatternState<State1, State2, T1, T2, T3>, T3>
+    for CouplePattern<Event, State1, State2, T1, T2, T3>
+where
+    State1: PatternState<T1>,
+    State2: PatternState<T2>,
+{
+    fn initial_state(&self) -> CouplePatternState<State1, State2, T1, T2, T3> {
+        CouplePatternState {
+            left: Box::new(self.left.initial_state()),
+            right: Box::new(self.right.initial_state()),
+            queue: VecDeque::new(),
+            phantom1: PhantomData,
+            phantom2: PhantomData,
+        }
+    }
+
+    fn  apply(&self, old_state: CouplePatternState<State1, State2, T1, T2, T3>, events: Vec<Event>) -> CouplePatternState<State1, State2, T1, T2, T3> {
+        unimplemented!()
+    }
+}
+
+impl<State1, State2, T1: Clone, T2: Clone, T3: Clone> PatternState<T3>
+    for CouplePatternState<State1, State2, T1, T2, T3>
+where
+    State1: PatternState<T1>,
+    State2: PatternState<T2>,
+{
+    fn queue(&self) -> QI<T3> { self.queue.clone() }
+    fn copy_with_queue(&self, queue: QI<T3>) -> CouplePatternState<State1, State2, T1, T2, T3> {
+       CouplePatternState { left: self.left.clone(), right: self.right.clone(), queue, phantom1: self.phantom1, phantom2: self.phantom2 }
     }
 }
